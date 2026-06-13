0df38bec04a16d7251f918de5994bd37a7da262c
hazeedshaik123s <hazeedshaik123s@gmail.com>
2026-06-13 18:33:48 +0530

    fresh quiz deployment

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "401224830" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "827647364" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
